2694bf8b2583dce4d2a184657f8610994d92b90f
Gabriel <farmagiancotti10@gmail.com>
2020-10-07 20:18:48 -0300

    commit gabriel

--- Movimento_Cavalo/Program.cs
+++ Movimento_Cavalo/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 class Movimento_Cavalo
 {
 
-    // Classe para armazenar osdsds dados COMMIT ANDRE
+    // Classe para armazenar osdsds dados COMMIT ANDRE COMMIT GABRIEL
     public class cell
     {
         public int x, y;
